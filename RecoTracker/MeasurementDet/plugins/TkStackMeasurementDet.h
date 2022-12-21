c662a8a9f1cb6a3cd80f7b37dce90c48f003299d
Adinda de Wit <adinda.maite.de.wit@cern.ch>
2022-12-21 11:11:31 +0100

    Partial fix for DoubleSensGeomDets in MeasurementTrackerImpl

--- RecoTracker/MeasurementDet/plugins/TkStackMeasurementDet.h
+++ RecoTracker/MeasurementDet/plugins/TkStackMeasurementDet.h
@@ -5,6 +5,7 @@
 #include "TkPhase2OTMeasurementDet.h"
 
 #include "Geometry/CommonDetUnit/interface/StackGeomDet.h"
+#include "Geometry/CommonDetUnit/interface/DoubleSensGeomDet.h"
 #include "RecoLocalTracker/ClusterParameterEstimator/interface/PixelClusterParameterEstimator.h"
 
 #include "FWCore/Utilities/interface/Visibility.h"
@@ -14,6 +15,8 @@
 class TkStackMeasurementDet final : public MeasurementDet {
 public:
   TkStackMeasurementDet(const StackGeomDet* gdet, const PixelClusterParameterEstimator* cpe);
+  TkStackMeasurementDet(const DoubleSensGeomDet* gdet, const PixelClusterParameterEstimator* cpe);
+
   void init(const MeasurementDet* lowerDet, const MeasurementDet* upperDet);
 
   RecHitContainer recHits(const TrajectoryStateOnSurface&, const MeasurementTrackerEvent& data) const override;
